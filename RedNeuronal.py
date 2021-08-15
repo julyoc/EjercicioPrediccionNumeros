748dff76279b529b2277ae667c201090db999f00
Julio Castro <1j1u1l1i1o@gmail.com>
2021-08-15 17:26:38 -0500

    Complete program

--- RedNeuronal.py
+++ RedNeuronal.py
@@ -1,11 +1,14 @@
 import numpy as np
+from numpy.lib.function_base import append
 
 class RedNeuronal:
-    def __init__(self):
+    def __init__(self, cant_inter, factorApren):
+        self.cant_inter = cant_inter
+        self.factorApren = factorApren
         pass
     def setValues(self, x, y):
-        self.w = (np.random.randn(x, y)/2) - 0.4999
-        self.w0 = self.w
+        self.w = np.random.rand(x, y)*0.1 - 0.05
+        self.w0 = self.w.copy()
 
     def sigmoid(self, s):
         return (1/(1+np.exp(-s)))
@@ -17,15 +20,110 @@ class RedNeuronal:
         z0=np.dot(datox, self.w)
         z = self.sigmoid(z0)
         err = (z - datoy)*self.Deffsigmoid(z);
-        self.err = err
+        self.err = np.linalg.norm(err)
         self.w += l*err*z 
 
-    def train(self, cant_inter, factorApren, xData, yData):
+    def train(self, xData, yData):
         self.setValues(len(xData[0]), len(yData[0]))
-        for _ in range(cant_inter):
+        for _ in range(self.cant_inter):
             for i in range(len(xData)):
-                self.forward(xData[i], yData[i], factorApren)
+                self.forward(xData[i], yData[i], self.factorApren)
 
-    def predict(self, x):
+    def predictProba(self, x):
         z0=np.dot(x, self.w)
         return self.sigmoid(z0)
+
+    def predict(self, x):
+        z0 = self.predictProba(x)
+        #return np.round(z0)
+        return self.rounded(z0)
+    
+    def rounded(self, x):
+        u = x.copy()
+        for i, v in enumerate(x):
+            for j, vv in enumerate(v):
+                if vv >= 0.5:
+                    u[i][j] = 1
+                    continue
+                u[i][j] = 0
+        return u
+""""
+class Perseptron(object):
+    
+    def __init__(self) -> None:
+        super().__init__()
+        self.err = 1.0
+
+    def crearpeso(self, x, y):
+        return np.random.rand(x, y)*0.1 - 0.05
+
+    def setValues(self, x, y, layers):
+        pesos = []
+        pesos.append(self.crearpeso(x, layers[0]))
+        for i in range(len(layers) - 1):
+            pesos.append(self.crearpeso(layers[i], layers[i + 1]))
+
+        pesos.append(self.crearpeso(layers[len(layers) - 1], y))
+        self.w = np.array(pesos)
+        self.w0 = self.w.copy()
+        del pesos
+
+    def sigmoid(self, s):
+        return (1/(1+np.exp(-s)))
+    
+    def Deffsigmoid(self, s):
+        return (s*(1-s))
+
+    def forward(self, datoX, itera):
+        z0 = np.dot(datoX, self.w[itera])
+        z = self.sigmoid(z0)
+        return z
+
+    def backward(self, z, datoY, itera, L):
+        if itera == len(self.w) - 1:
+             self.err = (datoY - z)*self.Deffsigmoid(z)
+             self.w[itera] += L*self.err*z
+             return
+        self.err = np.dot(self.sigmoid(z), self.w[itera].T)*self.err
+        er, z1 = self.err.reshape((self.err.shape[0], 1)), z.reshape((z.shape[0],1))
+        #print(self.w[itera].shape, '=', self.err.T.shape, 'x', z.T.shape)
+        print("i =", itera, "; ", self.w[itera].shape, '=', er.shape, 'x', z1.T.shape)
+        #self.w[itera] += np.dot(self.err.T, z.T)*L
+        self.w[itera] += np.dot(er, z1.T)*L
+
+    def train(self, Xdata, Ydata, iteracciones: int, layers = [], factorDeAprendizaje = 0.001) -> None:
+        self.setValues(len(Xdata[0]), len(Ydata[0]), layers)
+        for _ in range(iteracciones):
+            for j, value in enumerate(Xdata):
+                z = value
+                for i in range(len(self.w)):
+                    z = self.forward(z, i)
+                    self.backward(z, Ydata[j], i, factorDeAprendizaje)
+
+    def predict(self, Xdata):
+        ret = Xdata
+        for i in range(len(self.w)):
+            ret = self.forward(ret, i)
+        return ret
+
+
+x = np.array([[1,1,0,1], [1,0,1,1], [0,1,1,1], [0,1,0,1]])
+y = np.array([[1], [1], [0], [0]])
+model = Perseptron()
+model.train(x, y, 100, [3,2])
+ret = model.predict([1,1,0])
+print(ret, model.err)
+"""
+
+### Test Class
+"""
+model = RedNeuronal(1000, 0.001)
+x = np.array([[1,1,0,1], [1,0,1,1], [0,1,1,1], [0,1,0,1]])
+y = np.array([[1], [1], [0], [0]])
+model.train(x, y)
+print(model.predict([[1,1,0,1]]))
+print(model.predictProba([[1,1,0,1]]))
+print('pesos iniciales: ', model.w0)
+print('pesos calculados: ', model.w)
+print('error: ', model.err)
+"""
--- graficar.py
+++ graficar.py
@@ -6,6 +6,10 @@ def graficar(alldata):
     plt.spy(np.reshape(alldata[int(inp)], [30, 20]))
     plt.show()
 
+def graficar(alldata, inp):
+    plt.spy(np.reshape(alldata[int(inp)], [30, 20]))
+    plt.show()
+
 def graficarDT(dt):
     plt.spy(np.reshape(dt, [30, 20]))
     plt.show()
@@ -15,9 +19,29 @@ def graficarGrupo100(alldata):
     fig, axs = plt.subplots(10, 10)
     fig.set_size_inches(20, 30)
     k = int(inp)*100 -100
-    print(k)
     for i in range(10):
         for j in range(10):
+            axs[j, i].spy(np.reshape(alldata[k], [30, 20]))
+            k += 1
+    plt.show()
+
+def graficarGrupo100(alldata, inp):
+    fig, axs = plt.subplots(10, 10)
+    fig.set_size_inches(20, 30)
+    k = int(inp)*100 -100
+    for i in range(10):
+        for j in range(10):
+            #print((j, i))
+            axs[j, i].spy(np.reshape(alldata[k], [30, 20]))
+            k += 1
+    plt.show()
+
+def graficarPredData(alldata):
+    fig, axs = plt.subplots(4, 10)
+    fig.set_size_inches(20, 30)
+    k = 0
+    for i in range(10):
+        for j in range(4):
             axs[j, i].spy(np.reshape(alldata[k], [30, 20]))
             k += 1
     plt.show()
